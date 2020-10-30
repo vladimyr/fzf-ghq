6d6b276230754ac713326b089fd6844bc8ff306f
Dario Vladovic <d.vladimyr@gmail.com>
2020-10-30 19:05:46 +0100

    Follow zsh widget naming convention

--- fzf-ghq.plugin.zsh
+++ fzf-ghq.plugin.zsh
@@ -1,21 +1,21 @@
 _ghq_look() {
   ghq list | fzf +m -q "$1" \
     --preview-window up \
-    --preview "glow -p -s dark '$root/$(printf "%s" {})/README.md' 2> /dev/null ||
+    --preview "glow -p -s dark '\$(ghq root)/{}/README.md' 2> /dev/null ||
                echo 'No README file found'"
 }
 
-function ghq_cd() {
-  local root=$(ghq root)
-  local repo=$(_ghq_look "$LBUFFER")
+function fzf-ghq-widget() {
+  local repo
+  repo=$(_ghq_look "$LBUFFER")
 
   if [[ -n $repo ]]; then
-    BUFFER="cd $root/$repo"
+    BUFFER="cd $(ghq root)/$repo"
     zle accept-line
   fi
 
   zle reset-prompt
 }
 
-zle -N ghq_cd
-bindkey "^g" ghq_cd
+zle -N fzf-ghq-widget
+bindkey "^g" fzf-ghq-widget
